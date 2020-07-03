72600871d37f551bc776f9e5337f175c5563fa20
hamsal <hamza>
2020-07-03 08:36:26 +0200

    MainActivity added

---
+++ app/src/main/java/ch/zli/m335/movetoimprove/Clustermarker.java
@@ -0,0 +1,60 @@
+package ch.zli.m335.movetoimprove;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterItem;
+
+ public class Clustermarker implements ClusterItem {
+    private LatLng position;
+    private String title;
+    private String snippet;
+    private int iconPicture;
+
+     public Clustermarker(LatLng position, String title, String snippet, int iconPicture) {
+         this.position = position;
+         this.title = title;
+         this.snippet = snippet;
+         this.iconPicture = iconPicture;
+     }
+     public Clustermarker() {
+
+     }
+
+     @Override
+     public LatLng getPosition() {
+         return position;
+     }
+
+     @Override
+     public String getTitle() {
+         return title;
+     }
+
+     @Override
+     public String getSnippet() {
+         return snippet;
+     }
+
+     public int getIconPicture() {
+         return iconPicture;
+     }
+
+     public void setPosition(LatLng position) {
+         this.position = position;
+     }
+
+     public void setTitle(String title) {
+         this.title = title;
+     }
+
+     public void setSnippet(String snippet) {
+         this.snippet = snippet;
+     }
+
+     public void setIconPicture(int iconPicture) {
+         this.iconPicture = iconPicture;
+     }
+ }
+
--- app/src/main/java/ch/zli/m335/movetoimprove/TimeActivity.java
+++ app/src/main/java/ch/zli/m335/movetoimprove/TimeActivity.java
@@ -2,18 +2,69 @@ package ch.zli.m335.movetoimprove;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.text.format.Time;
 import android.widget.TextView;
 
 public class TimeActivity extends AppCompatActivity {
     private TextView countdownText;
 
     private CountDownTimer countDownTimer;
-    private long timeleftinmillisec =
+    private long TimeLeftInMilliSeconds = 86400000;
+    private boolean TimerRunning;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+        countdownText = findViewById(R.id.countdown_text);
+        startTimer();
+    }
+    public void startTimer() {
+        countDownTimer = new CountDownTimer(TimeLeftInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long l) {
+                TimeLeftInMilliSeconds = l;
+                updateTimer();
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
+    private void updateTimer() {
+        int hour = (int) TimeLeftInMilliSeconds / 3600000;
+        int minute = (int) TimeLeftInMilliSeconds % 3600000 / 60000;
+        int seconds = (int) TimeLeftInMilliSeconds % 60000 / 1000;
+        String timeleft;
+
+        timeleft = "" + hour;
+        timeleft +=  ":";
+        timeleft += "" + minute;
+        timeleft +=  ":";
+        if (seconds < 10 ) timeleft += "0";
+        timeleft += seconds;
+        countdownText.setText(timeleft);
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+        TimeLeftInMilliSeconds = prefs.getLong("millisLeft", 86400000);
+        TimerRunning = prefs.getBoolean("timerRunning", false);
+        updateTimer();
+        if (TimerRunning) {
+            if (TimeLeftInMilliSeconds < 0) {
+                TimeLeftInMilliSeconds = 0;
+                updateTimer();
+            } else {
+                startTimer();
+            }
+        }
     }
 }
